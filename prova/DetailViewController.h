9facebf7894f4c93a82de22e6913e34c6c807f40
Mirko <mirko@mirko-imac.locale.system-data.it>
2011-11-25 16:43:10 +0100

    Initial Commit

---
+++ prova/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  prova
+//
+//  Created by MIRKO COLLECCHI on 25/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
---
+++ prova/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  prova
+//
+//  Created by MIRKO COLLECCHI on 25/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
